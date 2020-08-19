ba35ef45b710105b370ddd5b6ce57d2167bfbb4f
Zachary Wilson <wilsonzachary634@gmail.com>
2020-08-19 15:10:30 -0600

    precommit

--- src/CsetAnalytics/src/app/components/user-management/user-management.service.ts
+++ src/CsetAnalytics/src/app/components/user-management/user-management.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from "@angular/common/http";
 import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { share } from "rxjs/operators";
-import { RegisterUser } from 'src/app/models/register-user.model';
-import { ConfigService } from '../../services/config.service';
+import { RegisterUser } from "src/app/models/register-user.model";
+import { ConfigService } from "../../services/config.service";
 
 @Injectable()
-
 export class UserManagementService {
-
   apiUrl: string;
-  constructor(private http: HttpClient, private router: Router, public configSvc: ConfigService) {
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    public configSvc: ConfigService
+  ) {
     this.apiUrl = configSvc.apiUrl;
   }
 
   public postCreateUser(user: RegisterUser): Observable<any> {
-    return this.http.post(this.apiUrl + 'user/createuser',user).pipe(share());
+    return this.http.post(this.apiUrl + "user/createuser", user).pipe(share());
   }
 }
