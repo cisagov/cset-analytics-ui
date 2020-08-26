0cf22430d0791358a256c624e2881578a1318fc6
Zachary Wilson <wilsonzachary634@gmail.com>
2020-08-26 10:21:02 -0600

    remove iam cert

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,10 +2,6 @@ data "aws_lb" "public" {
   name = "${var.app}-${var.env}-public-alb"
 }
 
-data "aws_iam_server_certificate" "self" {
-  name = "${var.app}-${var.env}-alb"
-}
-
 data "aws_security_group" "alb" {
   name = "${var.app}-${var.env}-alb"
 }
